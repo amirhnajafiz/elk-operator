61ccb6effa308541e9d6da5e834196234e40afcd
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-18 16:45:44 +0330

    init: elk api

---
+++ api/v1alpha1/elkcluster_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ElkClusterSpec defines the desired state of ElkCluster
+type ElkClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ElkCluster. Edit elkcluster_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ElkClusterStatus defines the observed state of ElkCluster
+type ElkClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ElkCluster is the Schema for the elkclusters API
+type ElkCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ElkClusterSpec   `json:"spec,omitempty"`
+	Status ElkClusterStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ElkClusterList contains a list of ElkCluster
+type ElkClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ElkCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ElkCluster{}, &ElkClusterList{})
+}
