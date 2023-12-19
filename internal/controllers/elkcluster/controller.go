82ccf46a93bbee9fb0a31ab0519cd6eff6da3309
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-19 11:34:42 +0330

    add: handlers files

--- internal/controllers/elkcluster/controller.go
+++ internal/controllers/elkcluster/controller.go
@@ -17,48 +17,11 @@ limitations under the License.
 package elkcluster
 
 import (
-	"context"
 	v1 "k8s.io/api/core/v1"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
 	monitoringv1alpha1 "github.com/amirhnajafiz/elk-operator/api/v1alpha1"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
-
-// Reconciler reconciles a ElkCluster object
-type Reconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
-}
-
-//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=monitoring.snappcload.io,resources=elkclusters,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=monitoring.snappcload.io,resources=elkclusters/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=monitoring.snappcload.io,resources=elkclusters/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// Modify the Reconcile function to compare the state specified by
-// the ElkCluster object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
-func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
-	// TODO: get elk cluster resource
-	// TODO: if deleted (clean up) => remove users from db, remove manifests
-	// TODO: check if exists (prevent creating)
-	// TODO: create Elk clusters with Kibana
-
-	return ctrl.Result{}, nil
-}
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
--- internal/controllers/elkuser/controller.go
+++ internal/controllers/elkuser/controller.go
@@ -17,46 +17,9 @@ limitations under the License.
 package elkuser
 
 import (
-	"context"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
 	monitoringv1alpha1 "github.com/amirhnajafiz/elk-operator/api/v1alpha1"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
-
-// Reconciler reconciles a ElkUser object
-type Reconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
-}
-
-//+kubebuilder:rbac:groups=monitoring.snappcload.io,resources=elkusers,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=monitoring.snappcload.io,resources=elkusers/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=monitoring.snappcload.io,resources=elkusers/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the ElkUser object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
-func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
-	// TODO: get user
-	// TODO: if deleted => remove from database
-	// TODO: if existed => prevent
-	// TODO: if not create a new user and insert it into db
-
-	return ctrl.Result{}, nil
-}
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
