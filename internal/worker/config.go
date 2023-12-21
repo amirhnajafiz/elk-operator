5cd5ccedb56979c4843f8aaa5d1e014edf993a2a
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-21 16:20:33 +0330

    add: worker

--- internal/worker/config.go
+++ internal/worker/config.go
@@ -1,1 +1,6 @@
 package worker
+
+type Config struct {
+	Enable   bool `koanf:"enable"`
+	Interval int  `koanf:"interval"`
+}
--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,1 +1,26 @@
 package worker
+
+import (
+	"database/sql"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type Worker struct {
+	Interval time.Duration
+	DB       *sql.DB
+	Logger   *zap.Logger
+}
+
+func (w Worker) Do() {
+	w.Logger.Info("database garbage collector started ...")
+
+	for {
+		if _, err := w.DB.Query("delete from users where deleted_at is not null"); err != nil {
+			w.Logger.Error("failed to run delete query", zap.Error(err))
+		}
+
+		time.Sleep(w.Interval)
+	}
+}
