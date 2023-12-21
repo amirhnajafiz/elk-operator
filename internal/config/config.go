94ac5865b43f5447e3e0c952bf02bcc16d2d0971
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-21 12:24:15 +0330

    init: config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,7 @@
 package config
+
+import "github.com/amirhnajafiz/elk-operator/internal/storage"
+
+type Config struct {
+	Database storage.Config `koanf:"database"`
+}
--- internal/config/default.go
+++ internal/config/default.go
@@ -1,1 +1,9 @@
 package config
+
+import "github.com/amirhnajafiz/elk-operator/internal/storage"
+
+func Default() Config {
+	return Config{
+		Database: storage.Config{},
+	}
+}
