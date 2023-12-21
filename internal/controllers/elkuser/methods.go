282ddd6b48c725dc49f9674fd51e45c14b2b507d
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-21 16:51:38 +0330

    init: elk user methods

---
+++ internal/controllers/elkuser/methods.go
@@ -0,0 +1,13 @@
+package elkuser
+
+func (r *Reconciler) create() error {
+	return nil
+}
+
+func (r *Reconciler) update() error {
+	return nil
+}
+
+func (r *Reconciler) delete() error {
+	return nil
+}
