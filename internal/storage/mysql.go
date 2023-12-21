113957c0df4ee47dedb1c43449fc327eb61b6bb4
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-21 12:22:07 +0330

    feat: db connection

--- internal/storage/mysql.go
+++ internal/storage/mysql.go
@@ -2,12 +2,17 @@ package storage
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func OpenConnection() (*sql.DB, error) {
-	db, _ := sql.Open("mysql", "dellis:@/shud")
+// OpenConnection to a mysql connection
+func OpenConnection(cfg Config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.DNS())
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
 
 	return db, nil
 }
