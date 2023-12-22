4936b6e41df29c221ea7b6786d984139ebb4efd6
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-22 18:14:55 +0330

    init: elk user components

--- internal/controllers/elkuser/provisioner.go
+++ internal/controllers/elkuser/provisioner.go
@@ -1,1 +1,12 @@
 package elkuser
+
+import (
+	"context"
+
+	"github.com/opdev/subreconciler"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func (r *Reconciler) Provision(ctx context.Context) (ctrl.Result, error) {
+	return subreconciler.Evaluate(subreconciler.DoNotRequeue())
+}
