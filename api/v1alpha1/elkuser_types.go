94d092458acfb5d1740275fb6a770a4c81bc8f48
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-24 09:18:57 +0330

    update: elk user spec status

--- api/v1alpha1/elkuser_types.go
+++ api/v1alpha1/elkuser_types.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
@@ -28,23 +12,27 @@ type ElkUserSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Username of the Elk cluster user
+	// Username of the Elk user (must be unique)
 	// +kubebuilder:validation:MaxLength:=16
 	Username string `json:"username,omitempty"`
 
-	// Password of the Elk cluster user
+	// Password of the Elk user
 	// +kubebuilder:validation:MaxLength:=16
 	// +kubebuilder:validation:MinLength:=8
 	Password string `json:"password,omitempty"`
 
-	// Roles of the Elk cluster user
+	// Roles of the Elk user
 	Roles []string `json:"roles,omitempty"`
+
+	// Clusters to register access for Elk user
+	Clusters []string `json:"clusters,omitempty"`
 }
 
 // ElkUserStatus defines the observed state of ElkUser
 type ElkUserStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=false
+	Created bool `json:"created,omitempty"`
 }
 
 //+kubebuilder:object:root=true
