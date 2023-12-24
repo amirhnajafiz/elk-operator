8de9f8593bf787d1b06ee19460f63c11114a959c
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-24 09:30:30 +0330

    add: users query

---
+++ pkg/query/users.go
@@ -0,0 +1,27 @@
+package query
+
+import "fmt"
+
+const UsersTable = "users"
+
+func queryInsertUser(username, password, roles, clusters string) string {
+	return fmt.Sprintf(
+		"INSERT INTO %s (username, password, roles, clusters) VALUES ('%s', '%s', '%s', '%s');",
+		UsersTable,
+		username,
+		password,
+		roles,
+		clusters,
+	)
+}
+
+func queryUpdateUser(old, username, roles, clusters string) string {
+	return fmt.Sprintf(
+		"UPDATE %s SET username='%s', roles='%s', clusters='%s' WHERE username='%s';",
+		UsersTable,
+		username,
+		roles,
+		clusters,
+		old,
+	)
+}
