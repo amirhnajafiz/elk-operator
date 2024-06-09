1a0d27d30330e7741ffb3d7574f1b6151488fe10
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-09 19:28:04 +0330

    remove: old files

--- api/v1alpha1/errors.go
+++
@@ -1,9 +0,0 @@
-package v1alpha1
-
-import "errors"
-
-var (
-	ErrUsernameExists = errors.New("cannot register your user, username is duplicated")
-	ErrUserNotFound   = errors.New("user with the requested username not found")
-	ErrReplicaNumber  = errors.New("cannot set even values for replica number, use odd numbers")
-)
--- internal/worker/config.go
+++
@@ -1,6 +0,0 @@
-package worker
-
-type Config struct {
-	Enable   bool `koanf:"enable"`
-	Interval int  `koanf:"interval"`
-}
