c5e3db31d87af3fb76be0185ce8f33c83dbcec58
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-11 19:13:40 +0330

    add: provisioner

---
+++ controllers/elk/provisioner.go
@@ -0,0 +1,25 @@
+package controllers
+
+func (r *Reconciler) ProvideConfigmaps() error {
+	return nil
+}
+
+func (r *Reconciler) ProvideElasticsearch() error {
+	return nil
+}
+
+func (r *Reconciler) ProvideLogstash() error {
+	return nil
+}
+
+func (r *Reconciler) ProvideFilebeat() error {
+	return nil
+}
+
+func (r *Reconciler) ProvideKibana() error {
+	return nil
+}
+
+func (r *Reconciler) ProvideServices() error {
+	return nil
+}
