94b4b79966786a7e8e0f443a7011f754687422da
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-11 16:41:49 +0330

    update: controller reconcile method

--- controllers/elk/controller.go
+++ controllers/elk/controller.go
@@ -3,10 +3,13 @@ package controllers
 import (
 	"context"
 
+	"github.com/go-logr/logr"
+	"github.com/opdev/subreconciler"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	monitoringamirhnajafizgithubcomv1beta1 "github.com/amirhnajafiz/elk-operator/api/v1beta1"
 )
@@ -14,7 +17,9 @@ import (
 // ELKReconciler reconciles a ELK object
 type ELKReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme   *runtime.Scheme
+	logger   logr.Logger
+	instance *monitoringamirhnajafizgithubcomv1beta1.ELK
 }
 
 //+kubebuilder:rbac:groups=monitoring.amirhnajafiz.github.com,resources=elks,verbs=get;list;watch;create;update;patch;delete
@@ -24,7 +29,15 @@ type ELKReconciler struct {
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 func (r *ELKReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	// set logger
+	r.logger = log.FromContext(ctx)
+
+	// trying to get ELK instance
+	err := r.Get(ctx, req.NamespacedName, r.instance)
+	if err != nil {
+		r.logger.Error(err, "")
+		return subreconciler.Evaluate(subreconciler.Requeue())
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -33,5 +46,6 @@ func (r *ELKReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 func (r *ELKReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&monitoringamirhnajafizgithubcomv1beta1.ELK{}).
+		WithEventFilter(predicate.GenerationChangedPredicate{}).
 		Complete(r)
 }
