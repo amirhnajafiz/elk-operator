2b80410c6b48f04f1fb3605260a6d97b68f62072
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-18 12:55:57 +0330

    add: new configmap resource generator

--- controllers/elk/resources/configmaps.go
+++ controllers/elk/resources/configmaps.go
@@ -1,7 +1,23 @@
 package resources
 
-func ElasticsearchConfigmapResource(namespace string) {
+import (
+	corev1 "k8s.io/api/core/v1"
+)
 
+// newConfigmapResource returns a default configmap resource
+func newConfigmapResource(name, namespace string) *corev1.ConfigMap {
+	cmp := &corev1.ConfigMap{}
+
+	cmp.Namespace = namespace
+	cmp.Name = name
+
+	return cmp
+}
+
+func ElasticsearchConfigmapResource(name, namespace string) *corev1.ConfigMap {
+	cmp := newConfigmapResource(name, namespace)
+
+	return cmp
 }
 
 func LogstashConfigmapResource(namespace string) {
